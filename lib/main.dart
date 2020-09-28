d7aefdaa2e19c639ed97e3ec4a70d206fff5109f
kota_koyama <kouta103177@gmail.com>
2020-09-28 09:28:31 +0900

    StatelessWidgetを記述

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,29 @@ import 'package:english_words/english_words.dart';
 void main() {
   runApp(MyApp());
 }
-
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: ' ようこそFlutterへ！',
+      title: ' ようこそ Flutterへ！',
       home: Scaffold(
         appBar: AppBar(
-            title: Text('Welcom to Flutter')
+            title: Text('Welcom to Flutter!!')
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
