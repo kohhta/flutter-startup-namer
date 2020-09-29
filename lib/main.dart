7a5a66f31e20e49ae90ad4b0ee977dcf9cbfb153
kota_koyama <kouta103177@gmail.com>
2020-09-29 23:09:02 +0900

    UIのベーステーマの変更

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      theme: ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
